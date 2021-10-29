ac6be6b04a9ec95c1c1cc1283c65c86ca22424a9
Jame <enzosuname@gmail.com>
2021-10-29 08:57:34 -0400

    started coding movement

--- movement.py
+++ movement.py
@@ -4,10 +4,12 @@ import random as r
 
 # Create color constants
 
+BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 GRAY = (92, 94, 92)
+DRKGRAY = (69, 66, 66)
 
 # Game constants
 
@@ -31,10 +33,13 @@ class Box():
         self.speed_y = r.randint(3, 5)
 
     def draw_ship(self):
-        g.draw.ellipse(self.display, self.color, (self.x, self.y, player_width, player_height))
+        g.draw.ellipse(self.display, self.color, (self.x, self.y, self.width, self.height))
 
     def draw_box(self):
-        g.draw.rect(self.display, self.color, (self.x, self.y, self.width, self.height))
+        g.draw.ellipse(self.display, self.color, [self.x, self.y, self.width, self.height])
+        g.draw.rect(screen, BLACK, [self.x, self.y, self.width, self.height-22])
+
+        #g.draw.rect(self.display, self.color, (self.x, self.y, self.width, self.height))
 
     def drop_box(self):
         if self.y > DISPLAY_LENGTH:
@@ -76,15 +81,16 @@ player_width = 25
 player_height = 100
 x_loc = (DISPLAY_WIDTH-player_width)/2
 y_loc = DISPLAY_LENGTH - 2*player_width
-player = Box(screen, x_loc, y_loc, player_width, player_width, GRAY)
+player = Box(screen, x_loc, y_loc, player_width, player_height, GRAY)
 
 # create enemies
-enemy_width = 20
+enemy_width = 15
+enemy_height = 45
 enemy_list=[]
 for i in range(10):
     x_coord = r.randrange(0, DISPLAY_WIDTH, 5)
     random_y = r.randrange(-100, 0, 5)
-    enemy_list.append(Box(screen, x_coord, random_y, enemy_width, enemy_width, RED))
+    enemy_list.append(Box(screen, x_coord, random_y, enemy_width, enemy_height, BLACK))
 
 g.mouse.set_visible(True)
 
@@ -110,6 +116,11 @@ while running:
             running = False
 
     player.draw_ship()
+
+    explosion = g.image.load('rename.png').convert_alpha()
+    #explosion = g.transform.scale(gun, [60, 30])
+    screen.blit(explosion, [0, 57.5])
+
     player.update()
 
     g.display.flip()
